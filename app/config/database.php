02d8bf143b93c6450c6c157d627ed02549efec7b
daniel <daniel@socialab.com>
2015-04-18 16:58:25 -0300

    funca database

--- app/config/database.php
+++ app/config/database.php
@@ -1,11 +1,5 @@
 <?php
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
 
 return array(
 
@@ -61,10 +55,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => $host,
-        	'database'  => $database,
-        	'username'  => $username,
-        	'password'  => $password,
+			'host'      => 'localhost',
+			'database'  => 'entel',
+			'username'  => 'root',
+			'password'  => 'root',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
